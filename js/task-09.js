901fb65dd51cadc8637a0ee97400545df9100faf
Putinjov <putinjov@gmail.com>
2023-02-28 22:52:00 +0200

    complete 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const refs = {
+  btnChangeColor: document.querySelector('.change-color'),
+  span: document.querySelector('.color'),
+};
+refs.btnChangeColor.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = getRandomHexColor()
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
